ea8dd6c70e7dbe2b494f440de312a7b8914c6484
Markus Ederl <markus.ederl@r-software.at>
2023-03-27 16:13:48 +0200

    Individually Colored Lines in Batch Script!

--- StartByClick.bat
+++ StartByClick.bat
@@ -1,11 +1,25 @@
 @echo off
-@REM Set the current Path, in which this file was executed to 'HOME'
+@REM Step 1 for individually colored Lines
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+@REM Setting current Path to 'HOME'
 set HOME=%~dp0%
-@REM Define absolute Path to your Powershell.exe
+@REM Define absolut Path to your Powershell-Directory
 set POWERSHELL_PATH = C:\Windows\System32\WindowsPowerShell\v1.0
-echo "####################"
-echo "Starting Batch Script"
-echo "####################"
+@REM Definiere Farbe des Outputs
+call :colorEcho 0a "####################"
+call :colorEcho 0a "Starting Batch Script"
+call :colorEcho 0a "####################"
 CD %POWERSHELL_PATH%
 Powershell -ExecutionPolicy Bypass -command "& '%HOME%./Powershell/HelloWorld.ps1' '%HOME%'"
 pause
+exit
+@REM Step 2 for individually colored Lines
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+echo:
